c75b223ce7942965d08262ba11f4e39fa84b5124
Zhomart <altynadamnn@gmail.com>
2025-02-26 09:43:16 +0600

    Update url.js

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,3 +1,2 @@
-export const URL= process.env.VITE_URL
-// eslint-disable-next-line no-undef
-export const IF= process.env.VITE_IF
+export const URL=import.meta.env.VITE_URL
+export const IF=import.meta.env.VITE_IF
