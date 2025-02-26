65771cde6eda4482f6fec240a1247f0323e98b15
Zhomart <altynadamnn@gmail.com>
2025-02-26 09:47:03 +0600

    Update verifyToken.js

--- backend/verifyToken.js
+++ backend/verifyToken.js
@@ -1,22 +1,21 @@
-const jwt=require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-const verifyToken=(req,res,next)=>{
-    const token=req.cookies.token
-    // console.log(token)
-    if(!token){
-        return res.status(401).json("You are not authenticated!")
+const verifyToken = (req, res, next) => {
+    const token = req.cookies.token; // Read token from cookies
+
+    if (!token) {
+        return res.status(401).json({ message: "You are not authenticated!" });
     }
-    jwt.verify(token,process.env.SECRET,async (err,data)=>{
-        if(err){
-            return res.status(403).json("Token is not valid!")
-        }
-        
-        req.userId=data._id
-       
-        // console.log("passed")
-        
-        next()
-    })
-}
 
-module.exports=verifyToken
+    jwt.verify(token, process.env.SECRET, (err, data) => {
+        if (err) {
+            return res.status(403).json({ message: "Token is not valid!" });
+        }
+
+        req.userId = data.userId;
+
+        next();
+    });
+};
+
+module.exports = verifyToken;
